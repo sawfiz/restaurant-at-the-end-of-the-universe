f4fa32e5ddc569c03dbe0f969bbd60ddbddaf062
sawfiz <dashu.up@gmail.com>
2022-10-28 20:27:20 +0800

    Hightlight active tab

--- src/index.js
+++ src/index.js
@@ -9,25 +9,36 @@ import './header.css';
 const contentEl = document.createElement('div');
 contentEl.setAttribute('id', 'content');
 document.body.appendChild(contentEl);
+
 loadHeader();
+const homeTab = document.querySelector('#home-tab')
+const menuTab = document.querySelector('#menu-tab')
+const reserveTab = document.querySelector('#reserve-tab')
 
 const mainEl = document.createElement('main');
 contentEl.appendChild(mainEl);
 
 loadHome();
+homeTab.classList.add('active')
 
 console.log('This is really working!');
 
-const homeTab = document.querySelector('#home-tab')
-const menuTab = document.querySelector('#menu-tab')
-const reserveTab = document.querySelector('#reserve-tab')
 
 homeTab.addEventListener('click', () => {
+    homeTab.classList.add('active')
+    menuTab.classList.remove('active')
+    reserveTab.classList.remove('active')
     loadHome();
 })
 menuTab.addEventListener('click', () => {
+    homeTab.classList.remove('active')
+    menuTab.classList.add('active')
+    reserveTab.classList.remove('active')
     loadMenu();
 })
 reserveTab.addEventListener('click', () => {
+    homeTab.classList.remove('active')
+    menuTab.classList.remove('active')
+    reserveTab.classList.add('active')
     loadReservation();
 })
