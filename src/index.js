4fabf98bd5a11fb7e9c034c8952f74108e52ad10
sawfiz <dashu.up@gmail.com>
2022-10-28 20:00:12 +0800

    Use HtmlWebpackPlugin to generate index.html

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 // const { default: loadHeader } = require('./header');
 
-// const contentEl = document.createElement('div');
-// contentEl.setAsstribute('id', 'content');
-
 import loadHeader from './header';
 import loadHome from './home';
 import './header.css';
+
+const contentEl = document.createElement('div');
+contentEl.setAttribute('id', 'content');
+document.body.appendChild(contentEl);
 
 loadHeader();
 loadHome();
--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-  // plugins: [
-  //   new HtmlWebpackPlugin({
-  //     title: 'Output Management',
-  //   }),
-  // ],
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
